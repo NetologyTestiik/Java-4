470b2d5651f41095b549bd99a19351c8c6702f42
Neto test <veronikakaravanova60@gmail.com>
2025-02-28 19:11:21 +0300

    change README

---
+++ src/BonusMilesServaice.java
@@ -0,0 +1,10 @@
+public class BonusMilesServaice {
+
+    public int calculate(int price) {
+        price = 10_000;
+        int mile = 20;
+        int miles = price / mile;
+
+        return miles;
+    }
+}
